c8f5dfa8fbc2fa6a7fae87feeb61d39c031bceff
Trevor Minnix <trevorminnix@knights.ucf.edu>
2017-03-19 19:53:45 -0400

    Added aliases to new jobs query

--- MetagenomicPipeline/src/metagenomePipeline/JobGetter.java
+++ MetagenomicPipeline/src/metagenomePipeline/JobGetter.java
@@ -38,33 +38,33 @@ public class JobGetter extends Thread{
 					String metaspadesAssembly, metaspadesReadMap, metaspadesStats, metaspadesVisual;
 					
 					//get info from query results
-					jobID = rs.getString("job.jobID");
-					pairedEnd = rs.getBoolean("job.pairedEnd");
-					idba = rs.getBoolean("job.idba");
-					megahit = rs.getBoolean("job.megahit");
-					metaspades = rs.getBoolean("job.metaspades");
+					jobID = rs.getString("jobID");
+					pairedEnd = rs.getBoolean("pairedEnd");
+					idba = rs.getBoolean("idba");
+					megahit = rs.getBoolean("megahit");
+					metaspades = rs.getBoolean("metaspades");
 					
-					trimParam = rs.getString("job.trimParam");
-					idbaParam = rs.getString("idba.param");
-					megahitParam = rs.getString("megahit.param");
-					metaspadesParam = rs.getString("metaspades.param");
+					trimParam = rs.getString("trimParam");
+					idbaParam = rs.getString("idbaParam");
+					megahitParam = rs.getString("megahitParam");
+					metaspadesParam = rs.getString("metaspadesParam");
 					
-					trimmed = rs.getString("job.trimmed");
+					trimmed = rs.getString("trimmed");
 					
-					idbaAssembly = rs.getString("idba.assembly");
-					idbaReadMap = rs.getString("idba.readMap");
-					idbaStats = rs.getString("idba.stats");
-					idbaVisual = rs.getString("idba.visual");
+					idbaAssembly = rs.getString("idbaAssembly");
+					idbaReadMap = rs.getString("idbaReadmap");
+					idbaStats = rs.getString("idbaStat");
+					idbaVisual = rs.getString("idbaVisual");
 					
-					megahitAssembly = rs.getString("megahit.assembly");
-					megahitReadMap = rs.getString("megahit.readMap");
-					megahitStats = rs.getString("megahit.stats");
-					megahitVisual = rs.getString("megahit.visual");
+					megahitAssembly = rs.getString("megahitAssembly");
+					megahitReadMap = rs.getString("megahitReadmap");
+					megahitStats = rs.getString("megahitStat");
+					megahitVisual = rs.getString("megahitVisual");
 					
-					metaspadesAssembly = rs.getString("metaspades.assembly");
-					metaspadesReadMap = rs.getString("metaspades.readMap");
-					metaspadesStats = rs.getString("metaspades.stats");
-					metaspadesVisual = rs.getString("metaspades.visual");
+					metaspadesAssembly = rs.getString("metaspadesAssembly");
+					metaspadesReadMap = rs.getString("metaspadesReadmap");
+					metaspadesStats = rs.getString("metaspadesStat");
+					metaspadesVisual = rs.getString("metaspadesVisual");
 					
 					//instantiate new job and enqueue in pipeline
 					MetagenomeJob j = new MetagenomeJob(jobID, pairedEnd, idba, megahit, metaspades, trimParam, 
