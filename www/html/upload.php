a316dce61db3f0dbd22dd07a9718dd2536d2a3a7
brvaeth <bvaeth@knights.ucf.edu>
2017-04-05 02:03:50 -0400

    Add files via upload

---
+++ www/html/upload.php
@@ -0,0 +1,23 @@
+<?php
+$target_dir ="/home/student/SeniorDesign-MetagenomicPipeline/FASTQ/Jobs";
+$target_file = $target_dir . basename($_FILES["my_file"]["name"]);
+$uploadOk = 1;
+
+if (file_exists($target_file)) {
+    echo "Sorry, file already exists.";
+    $uploadOk = 0;
+}
+
+// Check if $uploadOk is set to 0 by an error
+if ($uploadOk == 0) {
+    echo "Sorry, your file was not uploaded.";
+
+// if everything is ok, try to upload file
+} else {
+    if (move_uploaded_file($_FILES["my_file"]["tmp_name"], $target_file)) {
+        echo "The file ". basename( $_FILES["my_file"]["name"]). " has been uploaded.";
+    } else {
+        echo "Sorry, there was an error uploading your file.";
+    }
+}
+?>
