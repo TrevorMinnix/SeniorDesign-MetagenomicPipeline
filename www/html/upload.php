0f156a4d75872a01b17e6a730699b83f23d58fd0
student <TrevorMinnix>
2017-04-10 13:00:09 -0400

    Upload with session

--- www/html/upload.php
+++ www/html/upload.php
@@ -1,7 +1,12 @@
 <?php
-$target_dir ="/home/student/SeniorDesign-MetagenomicPipeline/FASTQ/Jobs/";
-$target_file = $target_dir . basename($_FILES["my_file"]["name"]);
+session_start();
+$chunk = isset($_REQUEST["chunk"]) ? intval($_REQUEST["chunk"]) : 0;
+$chunks = isset($_REQUEST["chunks"]) ? intval($_REQUEST["chunks"]) : 0;
+
+$target_dir ="/home/student/SeniorDesign-MetagenomicPipeline/Jobs/";
+$target_file = $target_dir . basename($_FILES["my_file"]["name"]); 
 $uploadOk = 1;
+
 if (file_exists($target_file)) {
     echo "Sorry, file already exists.";
     $uploadOk = 0;
@@ -11,7 +16,6 @@ if ($uploadOk == 0) {
     echo "Sorry, your file was not uploaded.";
 // if everything is ok, try to upload file
 } else {
-    mkdir($target_file, 0777, true);
     if (move_uploaded_file($_FILES["my_file"]["tmp_name"], $target_file)) {
         echo "The file ". basename( $_FILES["my_file"]["name"]). " has been uploaded.";
     } else {
