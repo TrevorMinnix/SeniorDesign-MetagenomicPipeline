3516195690604b5319fdcd346f8c12a4b891f31c
student <TrevorMinnix>
2017-04-17 12:30:05 -0400

    idk

--- www/html/upload.php
+++ www/html/upload.php
@@ -36,9 +36,6 @@ echo $query;
 $con->query($query);
 
 mkdir("/home/student/SeniorDesign-MetagenomicPipeline/Jobs/" . $jobID . "/");
-mkdir("home/student/SeniorDesign-MetagenomicPipeline/Jobs/" . $jobID . "/" . "IDBA". "/");
-mkdir("home/student/SeniorDesign-MetagenomicPipeline/Jobs/" . $jobID . "/" . "MEGAHIT". "/");
-mkdir("home/student/SeniorDesign-MetagenomicPipeline/Jobs/" . $jobID . "/" . "MetaSPAdes". "/");
 $target_dir = "/home/student/SeniorDesign-MetagenomicPipeline/Jobs/" . $jobID . "/";
 
 //mkdir($target_dir . "IDBA". "/");
