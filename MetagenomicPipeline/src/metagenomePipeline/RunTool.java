3ef39f0df1e8c29c18fd4aefbed98405ccf8931c
TrevorMinnix <trevorminnix@knights.ucf.edu>
2017-04-21 10:46:16 -0400

    runtool prints output

--- MetagenomicPipeline/src/metagenomePipeline/RunTool.java
+++ MetagenomicPipeline/src/metagenomePipeline/RunTool.java
@@ -6,11 +6,6 @@ import java.io.InputStream;
 import java.io.InputStreamReader;
 
 public class RunTool {
-	static void runProgramAndWait(String program, String args) throws IOException, InterruptedException{
-		Process p = Runtime.getRuntime().exec(program + " " + args);
-		p.waitFor();
-	}
-	
 	static String runProgramAndWait(String command) throws IOException, InterruptedException{
 		System.out.println(command);
 		String[] splitCommand = command.split("\\s+");
@@ -20,7 +15,7 @@ public class RunTool {
 		InputStream is = process.getInputStream();
 		BufferedReader reader = new BufferedReader(new InputStreamReader(is));
 
-		process.waitFor();
+		//process.waitFor();
 		
 		String line = null;
 		String output = "";
