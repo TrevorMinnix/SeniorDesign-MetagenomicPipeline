c9ee983fda8d7fe83262a2b0d3b5a229362806ea
TrevorMinnix <trevorminnix@knights.ucf.edu>
2017-04-22 15:50:09 -0400

    formatting download

--- www/html/results.php
+++ www/html/results.php
@@ -201,7 +201,7 @@ float: left;
 <div id="as2" class="tabcontent" <?php if($row['idba'] == 1): ?>style="display: none"<?php endif; ?>>
   <h3>MEGAHIT</h3>
   <hr style="height:30px; color: black;">
-  <a id="mhhref" href="#" download>Download assembled file.</a>
+  <a id="mhhref" href="#" download><h3>Download assembled file.</h3></a>
   <ul>
 	<li>
 		<h3>Cumulative Plot</h3>
@@ -236,7 +236,7 @@ float: left;
 <div id="as3" class="tabcontent" <?php if($row['idba'] == 1 || $row['megahit'] == 1): ?>style="display: none"<?php endif; ?>>
   <h3>MetaSPAdes</h3>
   <hr style="height:30px; color: black;">
-  <a id="mshref" href="" download>Download assembled file.</a>
+  <a id="mshref" href="" download><h3>Download assembled file.</h3></a>
   <ul>
 	<li>
 		<h3>Cumulative Plot</h3>
