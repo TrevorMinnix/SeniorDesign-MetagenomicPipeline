d51c5bd1255d1f5d84322909be4b2e448b573027
TrevorMinnix <trevorminnix@knights.ucf.edu>
2017-04-22 20:52:43 -0400

    runtool

--- MetagenomicPipeline/src/metagenomePipeline/RunTool.java
+++ MetagenomicPipeline/src/metagenomePipeline/RunTool.java
@@ -1,9 +1,9 @@
 package metagenomePipeline;
 
-//import java.io.BufferedReader;
+import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStream;
-//import java.io.InputStreamReader;
+import java.io.InputStreamReader;
 
 public class RunTool {
 	static void runProgramAndWait(String program, String args) throws IOException, InterruptedException{
@@ -11,42 +11,16 @@ public class RunTool {
 		p.waitFor();
 	}
 	
-	static int runProgramAndWait(String command) throws IOException, InterruptedException{
+	static void runProgramAndWait(String command) throws IOException, InterruptedException{
 		System.out.println(command);
 		String[] splitCommand = command.split("\\s+");
 		final ProcessBuilder builder = new ProcessBuilder(splitCommand);
 		builder.redirectErrorStream(true);
 		final Process process = builder.start();
-		//InputStream is = process.getInputStream();
-		//BufferedReader reader = new BufferedReader(new InputStreamReader(is));
+		InputStream is = process.getInputStream();
+		BufferedReader reader = new BufferedReader(new InputStreamReader(is));
 
-		//process.waitFor();
-		
-		//String line = null;
-		//while ((reader.readLine()) != null) {
-		//}
-		
-		final Thread reader = new Thread(new Runnable() {
-			@Override
-			public void run(){
-				try{
-					final InputStream is = process.getInputStream();
-					int c;
-					while((c = is.read()) != 1){
-						
-					}
-				}catch(IOException e){
-					e.printStackTrace();
-				}
-			}
-		});
-		
-		reader.start();
-		
-		final int exitCode = process.waitFor();
-		reader.join();
-		
-		return exitCode;
+		while(reader.readLine() != null);	
 	}
 	
 	
